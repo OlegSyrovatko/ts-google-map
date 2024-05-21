afc4d466efe290599e774babb30737a47b3dac89
Oleg <sirov@ukr.net>
2024-05-21 23:01:12 +0300

    add map

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,9 @@ import axios from "axios";
 
 const myForm = document.querySelector("Form")! as HTMLFormElement;
 const myInput = document.getElementById("query")! as HTMLInputElement;
+// restricted by my Ip addresses
 const googleApi = "AIzaSyBMS69yFjuVahxcPiHroI3VRDMnfVk9pyc";
+declare var google: any;
 
 type GoogleResponceGeneric = {
   results: { geometry: { location: { lat: number; lng: number } } }[];
@@ -12,7 +14,6 @@ type GoogleResponceGeneric = {
 function clickHandler(e: Event) {
   e.preventDefault();
   const query = myInput.value;
-  console.log(query);
   axios
     .get<GoogleResponceGeneric>(
       `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
@@ -23,7 +24,14 @@ function clickHandler(e: Event) {
       if (res.data.status !== "OK") {
         throw new Error("Could not catch location!");
       }
-      console.log(res.data.results[0].geometry.location);
+      const coord = res.data.results[0].geometry.location;
+
+      const map = new google.maps.Map(document.getElementById("map"), {
+        center: coord,
+        zoom: 8,
+      });
+
+      new google.maps.Marker({ position: coord, map: map });
     })
     .catch((err) => {
       alert(err.message);
